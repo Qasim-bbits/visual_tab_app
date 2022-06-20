f69a60e43f2f3e6a8bff3787eb2c9108b0036645
Saaad007 <saad@bbits.solutions>
2022-06-20 20:15:39 +0500

    main branch

---
+++ visual_tab_app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'visual_tab_app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
